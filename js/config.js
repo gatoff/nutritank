5887ce2149edafe3f459d885f696ee3014eb6f72
Jack Djanogly <mrjackgd@gmail.com>
2018-12-28 00:47:08 +0000

    pre-show deploy 1

---
+++ js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'nutritank.auth0.com',
+        clientId: 'eOJ5WLYQLvUaxWR6PLOvGA0WOz8GF67_'
+    },
+    apiBaseUrl: 'https://amatfjawjb.execute-api.eu-west-1.amazonaws.com/dev'
+};
